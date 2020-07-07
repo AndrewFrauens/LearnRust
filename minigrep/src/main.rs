9245bcf0cf1cfb625d715a000dde84c5a31cdad4
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2020-07-06 21:44:24 -0500

    started separating concerns from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,24 @@
 use std::env;
+use std::fs;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[0];
+    let (query, filename) = parse_config(args);
 
     println!("Searching for : {}", query);
     println!("File : {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong when opening the file.");
+
+    println!("With the text :\n\n{}\n", contents);
+}
+
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
