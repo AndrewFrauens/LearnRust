5db3a680deba6bd450c0a3a415b4ffc486e96fc4
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2020-07-06 22:15:11 -0500

    Throw error on too few args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem encounterd while running: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for : {}", config.query);
     println!("File : {}", config.filename);
@@ -20,11 +24,15 @@ struct Config {
     filename: String,
 }
 
-impl Config{
-    fn new(args: &[String]) -> Config {
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("too few args.  expecting arguments of: <query> <filename>");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
