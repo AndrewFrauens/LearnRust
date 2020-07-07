38b5f6a4e05090c3f3ec685128dae0f5bcdf235e
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2020-07-06 21:15:28 -0500

    more tutorial stuff

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+struct Rectangle {
+    width: u32,
+    height: u32
 }
+
+
+fn main() {
+    let rect1 = Rectangle {
+                    width : 30,
+                    height : 35
+                    };
+
+    println!("The area of a rectangle {} by {} is {}.",
+             rect1.width,
+             rect1.height,
+             area(&rect1));
+
+}   
+
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
