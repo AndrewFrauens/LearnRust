63c13a30a08b4064184449a1843a5513cd6a7021
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2020-07-06 21:51:45 -0500

    created config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,24 +1,28 @@
 use std::env;
 use std::fs;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(args);
+    let config = parse_config(args);
 
-    println!("Searching for : {}", query);
-    println!("File : {}", filename);
+    println!("Searching for : {}", config.query);
+    println!("File : {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong when opening the file.");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong when opening the file.");
 
     println!("With the text :\n\n{}\n", contents);
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+fn parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let filename = &args[2];
 
-    (query, filename)
+    Config { query, filename }
 }
