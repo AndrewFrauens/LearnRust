2091fd0181f75901c6f0226ebbe753ccadc1565d
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2020-07-09 23:22:09 -0500

    Adjusted to use iterators instead of clone()

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,73 +1,66 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
-
 
 pub struct Config {
-   pub query: String,
-   pub filename: String,
-   pub case_sensitive: bool,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("too few args.  expecting arguments of: <query> <filename> <opt: CASE_INSENSITIVE as environement variable>");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next(); // First arg is name of file being called so it should be removed.
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not recieve query string input."),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not recieve filename string input."),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename, case_sensitive })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    let results = if config.case_sensitive{
+    let results = if config.case_sensitive {
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
     };
 
-
-    for line in results{
+    for line in results {
         println!("{}", line);
     }
 
     Ok(())
 }
 
-
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(&query){
-            results.push(line);
-
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query){
-            results.push(line);
-
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -86,7 +79,7 @@ Duct tape.";
     }
 
     #[test]
-    fn case_insensitive(){
+    fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
 Rust:
@@ -94,11 +87,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-
         assert_eq!(
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
     }
-
 }
